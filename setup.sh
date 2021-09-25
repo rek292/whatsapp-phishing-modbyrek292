075d9eb75652ef7e70f6093f70dc56ceb492355e
rek292 <83674893+rek292@users.noreply.github.com>
2021-09-25 14:53:47 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # setup ngrok
+echo ""
 sleep 0.5
 echo "[+] Running setup ...."
 sleep 0.5
+echo ""
 
 sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && sudo unzip ngrok-stable-linux-amd64.zip && sudo rm -rf ngrok-stable-linux-amd64.zip
 sudo chmod +x ngrok
+sudo apt-get install xterm
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install apache2 -y
 sudo rm -rf setup.sh
 clear
 exit
